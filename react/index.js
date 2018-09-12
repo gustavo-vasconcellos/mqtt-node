371d2fa08c7398f7280d582701f17937f3773031
Gustavo Vasconcellos <gustavo.vasconcellos@cupula.local>
2018-09-12 10:46:45 -0300

    Alterações de Funcionalidade

--- react/index.js
+++ react/index.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { Connector } from 'mqtt-react'
+import React, { Component } from 'react'
+import mqtt from 'mqtt'
 
-import MessageList from './subscribe'
+const client = mqtt.connect('tcp://mqtt-broker.vtex.com.br')
 
-export default function HelloWorld() {
-	return (
-		<Connector mqttProps="tcp://mqtt-broker.vtex.com.br/">
-			<MessageList />
-		</Connector>
-	)
+client.on('connect', function() {
+    client.subscribe("vtex/falco/1/instore/checkin");
+});
+
+export default class HelloWorld extends Component {
+	render() {
+		return (
+			<div>
+				<MessageList />
+			</div>
+		)
+	}
+}
+
+class MessageList extends Component {
+	addMessage(message) {
+        var updated = this.state.messages;
+        updated.push(message);
+        this.setState({messages: updated});
+    }
+
+    getInitialState() {
+        return { messages: [] };
+    }
+
+    componentDidMount() {
+        var self = this;
+
+        client.on('message', function(topic, payload, packet) {
+            self.addMessage({
+                key: Date.now(),
+                topic: topic,
+                payload: payload.toString()
+            });
+        });
+    }
+
+    render() {
+        const messageNodes = this.state.messages.map(function(message) {
+            return (
+                <li key={message.key}>{message}</li>
+            );
+		});
+
+        return (
+            <div id="messageList" className="table-block footer-push">
+                <h1>Channel Name</h1>
+                <ul className="messages">{messageNodes}</ul>
+            </div>
+        );
+    }
 }
